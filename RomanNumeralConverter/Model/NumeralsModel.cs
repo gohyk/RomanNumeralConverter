d4579a689320f51a7a01d8c7e8568f922c1af3ed
Goh Yong Kiat <yongkiat92@gmail.com>
2021-06-21 23:21:17 +0800

    Add Model and View

---
+++ RomanNumeralConverter/Model/NumeralsModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+
+namespace RomanNumeralConverter.Model
+{
+    public class NumeralsModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private int arabic;
+        public int Arabic
+        {
+            get { return arabic; }
+            set { arabic = value; OnPropertyChanged("Arabic"); }
+        }
+
+        private int roman;
+        public int Roman
+        {
+            get { return roman; }
+            set { roman = value; OnPropertyChanged("Roman"); }
+        }
+    }
+}
