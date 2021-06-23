cec406cbf958a86ede338930be3985654a35249f
Goh Yong Kiat <yongkiat92@gmail.com>
2021-06-23 08:58:41 +0800

    Add ArabicToRoman logic

--- RomanNumeralConverter/ViewModels/NumeralsViewModel.cs
+++ RomanNumeralConverter/ViewModels/NumeralsViewModel.cs
@@ -1,10 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using RomanNumeralConverter.Model;
 
 namespace RomanNumeralConverter.ViewModels
 {
-    public class NumeralsViewModel
+    public class NumeralsViewModel : INotifyPropertyChanged
     {
         private NumeralsModel numeralsModel;
         public NumeralsViewModel()
@@ -22,21 +23,56 @@ namespace RomanNumeralConverter.ViewModels
             set 
             {
                 numeralsModel.Arabic = (value != "") ? Int64.Parse(value) : 0;
-                OnPropertyChanged("Arabic");
+                Roman = ArabicToRoman(numeralsModel.Arabic);
+                OnPropertyChanged(nameof(Arabic));
             }
         }
 
         public string Roman
         {
             get 
-            { 
+            {
                 return numeralsModel.Roman; 
             }
 
             set 
-            { 
-                numeralsModel.Roman = value; OnPropertyChanged("Roman"); 
+            {
+                numeralsModel.Roman = value;
+                OnPropertyChanged(nameof(Roman));
             }
+        }
+
+        private string ArabicToRoman(long num)
+        {
+            
+            var result = new List<string>();
+            var ValToSymbol = new Dictionary<int, string>
+            {
+                { 1000, "M" },
+                { 900, "CM" },
+                { 500, "D" },
+                { 400, "CD" },
+                { 100, "C" },
+                { 90, "XC" },
+                { 50, "L" },
+                { 40, "XL" },
+                { 10, "X" },
+                { 9, "IX" },
+                { 5, "V" },
+                { 4, "IV" },
+                { 1, "I" },
+            };
+
+            foreach (var pair in ValToSymbol)
+            {
+                while (num >= pair.Key)
+                {
+                    result.Add(pair.Value);
+                    num -= pair.Key;
+                }
+            }
+
+            return String.Concat(result);
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
